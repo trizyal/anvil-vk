5525e8cca6506d5900825460939f89b82145f344
TejaswaRizyal <rizyal.tejaswa@gmail.com>
2026-05-21 00:49:46 +0100

    file io

--- source/utilities/AnvilFileIO.cpp
+++ source/utilities/AnvilFileIO.cpp
@@ -1,1 +1,23 @@
 #include "AnvilFileIO.h"
+
+#include <fstream>
+#include <stdexcept>
+
+std::vector<char> AnvilFileIO::readFile(const std::string& filename)
+{
+    std::ifstream file(filename, std::ios::ate | std::ifstream::binary);
+
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Failed to open file " + filename);
+    }
+
+    const size_t fileSize = file.tellg();
+    std::vector<char> buffer(fileSize);
+
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    file.close();
+
+    return buffer;
+}
--- source/utilities/AnvilFileIO.h
+++ source/utilities/AnvilFileIO.h
@@ -1,10 +1,13 @@
 #ifndef ANVIL_VK_FILEIO_H
 #define ANVIL_VK_FILEIO_H
 
+#include <string>
+#include <vector>
 
 class AnvilFileIO
 {
+public:
+    static std::vector<char> readFile(const std::string &filename);
 };
 
-
-#endif //ANVIL_VK_ANVILFILEIO_H
+#endif //ANVIL_VK_FILEIO_H
