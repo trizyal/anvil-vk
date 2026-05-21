f640da89f019333b3c47616b4aca9a04599ed9ac
TejaswaRizyal <rizyal.tejaswa@gmail.com>
2026-05-21 20:10:44 +0100

    anvil renderer header setup

---
+++ source/vulkan/AnvilRenderer.h
@@ -0,0 +1,39 @@
+#ifndef ANVIL_VK_RENDERER_H
+#define ANVIL_VK_RENDERER_H
+
+#include "AnvilSwapchain.h"
+
+struct AnvilFrame
+{
+    VkCommandPool anvilCommandPool;
+    VkCommandBuffer anvilCommandBuffer;
+
+    // Sync objects
+    VkSemaphore anvilSwapchainSemaphore; // Image is ready to render to
+    VkSemaphore anvilRenderSemaphore; // Render is finished, ready to present
+    VkFence anvilRenderFence; // CPU waits for GPU to finish this frame
+};
+
+constexpr uint32_t FRAMES_IN_FLIGHT = 2;
+
+class AnvilRenderer
+{
+private:
+    AnvilVulkanContext* anvilContext;
+    AnvilSwapchain* anvilSwapchain;
+
+    AnvilFrame anvilFrames[FRAMES_IN_FLIGHT];
+    uint32_t anvilFrameNumber = 0;
+
+    AnvilFrame& getCurrentFrame();
+    void setupSyncStructures();
+    void setupCommandBuffers();
+
+public:
+    void initialise(AnvilVulkanContext* inAnvilContext, AnvilSwapchain* inAnvilSwapchain);
+    void cleanup();
+
+    void drawFrame();
+};
+
+#endif //ANVIL_VK_RENDERER_H
