ef3d767212d9f878a746ef98c62e4d81c10807e6
tej <rizyal.tejaswa@gmail.com>
2026-09-17 15:11:03 +0100

    Setting up pbr tests example

---
+++ examples/PBRTests/PBRTests.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2026 trizyal
+// SPDX-License-Identifier: GPL-3.0-only
+
+#ifndef ANVIL_VK_PBRTESTS_H
+#define ANVIL_VK_PBRTESTS_H
+
+
+class PBRTests
+{
+};
+
+
+#endif //ANVIL_VK_PBRTESTS_H
